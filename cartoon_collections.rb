77011aa3f071f03b614d3a381f790a45fa53998c
chic-state-0895 <brookeabryski@gmail.com>
2020-09-02 20:26:56 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,7 +22,7 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.include?(cheese_types)
+  array.include?("cheddar", "gouda", "camembert")
   return true
 else
   return nil
