bc8f29fb879de35e4a07dc5e92dfbf90359ad7e5
chic-state-0895 <brookeabryski@gmail.com>
2020-09-02 20:12:12 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,15 +1,16 @@
-dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
 
 def roll_call_dwarves(array)
- i = 0
+ index = 0
  array.each_with_index { |name, index|
    puts "#{index + 1}. #{name}"
-
  }
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  planeteer_calls = []
+  array.collect(&:capitalize)
+    
+  end
 end
 
 def long_planeteer_calls# code an argument here
