61fd887dde40905509d64f49effc320778df804f
chic-state-0895 <brookeabryski@gmail.com>
2020-09-02 20:03:01 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,11 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
+
+def roll_call_dwarves(array)
+ i = 0
+ array.each_with_index { |name, index|
+   puts "#{index + 1}. #{name}"
+
+ }
 end
 
 def summon_captain_planet# code an argument here
